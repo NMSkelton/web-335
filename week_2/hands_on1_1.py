80c202c67e5effe7d2d16931b2ac04193b7fd751
Nicholas Skelton <nicholasskelton30@gmail.com>
2026-01-25 13:05:19 -0600

    Week 2 Push

---
+++ week_2/hands_on1_1.py
@@ -0,0 +1,8 @@
+""" 
+    Title: hands_on1_1.py
+    Author: Nicholas Skelton
+    Date: 1 January 2026
+    Description: Hands-On 1.1: Building Your First Python Program
+"""
+
+print(""" "Spears shall be shaken! Shields shall be splintered! A sword-day! A red-day! Ere the sun rises!" - King Théoden of Rohan """)
